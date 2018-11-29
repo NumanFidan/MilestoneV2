58eb970870a7048fb9dd43d1f9867a4e89f204f0
NumanFidan <numanfidan@std.iyte.edu.tr>
2018-11-29 19:27:50 +0300

    Applied Realm and Retrofit

--- app/src/main/java/com/simplertutorials/android/milestonev2/data/api/ApiClient.java
+++ app/src/main/java/com/simplertutorials/android/milestonev2/data/api/ApiClient.java
@@ -7,12 +7,12 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ApiClient {
 
     private static Retrofit retrofit =null;
-    private static String POPULAR_MOVIES_BASE_URL= "https://api.themoviedb.org/";
+    private static String BASE_URL= "https://api.themoviedb.org/";
 
     public static Retrofit getClient(){
         if (retrofit == null){
             retrofit = new Retrofit.Builder()
-                    .baseUrl(POPULAR_MOVIES_BASE_URL)
+                    .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
                     .client(new OkHttpClient())
                     .build();
