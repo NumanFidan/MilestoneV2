cd97b7396de95992eb5cf734f2e641910a90e59a
NumanFidan <numanfidan@std.iyte.edu.tr>
2018-12-10 23:14:21 +0300

    Minor Adjustments

--- app/src/main/java/com/simplertutorials/android/milestonev2/ui/activities/BaseActivity.java
+++ app/src/main/java/com/simplertutorials/android/milestonev2/ui/activities/BaseActivity.java
@@ -1,6 +1,5 @@
 package com.simplertutorials.android.milestonev2.ui.activities;
 
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 
@@ -8,29 +7,12 @@ import com.simplertutorials.android.milestonev2.R;
 
 public class BaseActivity extends AppCompatActivity {
 
-    private ProgressDialog movieLoadingDialog;
-
     public Context getContext() {
         return this;
     }
 
     public String getLanguageString() {
         return getContext().getString(R.string.languageCodeForApı);
-    }
-
-
-    public void showProgressDialogToUser(String message) {
-        if (movieLoadingDialog == null)
-            movieLoadingDialog = new ProgressDialog(getContext());
-        movieLoadingDialog.setTitle(null);
-        movieLoadingDialog.setMessage(message);
-        movieLoadingDialog.setCancelable(false);
-        movieLoadingDialog.show();
-    }
-
-    public void dismissLoadingDialog() {
-        if (movieLoadingDialog != null)
-            movieLoadingDialog.dismiss();
     }
 
 }
